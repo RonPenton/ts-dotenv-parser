83babdd8b2f7a1acf6b54af348597f324e7feabb
rlp2 <ron.penton@ellucian.com>
2021-10-13 11:06:39 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,156 @@
+import { env } from 'process';
+
+type Parser<Out> = (val: string) => Out;
+
+type Option<Out> = {
+    parser?: Parser<Out>;
+    default?: Out;
+}
+
+type WithoutParser<Out> = {
+    default?: Out;
+}
+
+type WithParser<Out> = {
+    parser: Parser<Out>;
+    default?: Out;
+}
+
+type ValueType<O> =
+    O extends { parser: Parser<infer R> } ? R :
+    O extends { default: infer D } ? D
+    : string;
+
+type DotEnv<C extends DotEnvOptions> = {
+    [Key in keyof C]: ValueType<C[Key]>
+};
+
+type DotEnvOptions = {
+    [key: string]: Option<any>;
+};
+
+export function parseDotEnv<C extends DotEnvOptions>(configuration: C): DotEnv<C> {
+    const variables = Object.keys(configuration) as (keyof C)[];
+    const result = {} as DotEnv<C>;
+    const errors = [];
+
+    for (const variable of variables) {
+        const value = env[variable as string];
+        const config = configuration[variable];
+        if (value !== undefined && value.trim() !== "") {
+            try {
+                result[variable] = config.parser ? config.parser(value) : value;
+            } catch (e: any) {
+                errors.push(`failed to parse environment variable {${variable}}: "${e.message}">`);
+            }
+
+        } else {
+            if ('default' in config) {
+                result[variable] = config.default;
+            } else {
+                errors.push(`Environment variable {${variable}} is missing`);
+            }
+        }
+    }
+
+    if (errors.length > 0) {
+        throw new Error(errors.join('\n'));
+    }
+
+    return result;
+}
+
+const isNumber = (opts?: WithoutParser<number> & { min?: number, max?: number }): WithParser<number> => {
+
+    return {
+        parser: (val: string) => {
+            const f = parseFloat(val);
+            if (opts?.min !== undefined && f < opts.min) {
+                throw new Error(`Value is less than minium value: ${opts.min}`);
+            }
+            if (opts?.max !== undefined && f > opts.max) {
+                throw new Error(`Value is more than maximum value: ${opts.max}`);
+            }
+            if (isNaN(f)) {
+                throw new Error(`Value is not a number: ${val}`);
+            }
+            return f;
+        },
+        default: opts?.default
+    };
+}
+
+const isBoolean = (opts?: WithoutParser<boolean>): WithParser<boolean> => {
+    return {
+        parser: (val: string) => {
+            if (val.toLowerCase() == 'true' || val == '1') {
+                return true;
+            }
+            if (val.toLowerCase() == 'false' || val == '0') {
+                return false;
+            }
+            throw new Error(`Value is not a boolean: ${val}`);
+        },
+        default: opts?.default
+    };
+}
+
+const isEnum = <U extends string, T extends Readonly<[U, ...U[]]>>(vals: T, opts?: WithoutParser<T[number]>): WithParser<T[number]> => {
+
+    return {
+        parser: (val) => {
+            const lower = val.toLowerCase();
+            for (const original of vals) {
+                if (original.toLowerCase() == lower) {
+                    return original;
+                }
+            }
+
+            throw new Error(`Value is does not belong to any of the following: ${vals}`);
+        },
+        default: opts?.default
+    };
+}
+
+type Checker<T> = (obj: any) => obj is T;
+const isJson = <T>(checker: Checker<T>, opts?: WithoutParser<T>): WithParser<T> => {
+    return {
+        parser: (val) => {
+            const obj = JSON.parse(val);
+            if (checker(obj)) {
+                return obj;
+            }
+            throw new Error(`Value did not parse properly.`);
+        },
+        default: opts?.default
+    };
+}
+
+const isArray = <T>(checker: Checker<T>, opts?: WithoutParser<T[]>): WithParser<T[]> => {
+    return {
+        parser: (val) => {
+            const obj = JSON.parse(val);
+            if (Array.isArray(obj) && obj.every(checker)) {
+                return obj;
+            }
+            throw new Error(`Array did not parse properly.`);
+        },
+        default: opts?.default
+    };
+}
+
+const isString = (obj: any): obj is string => {
+    return typeof obj === 'string';
+}
+const isStringArray = (opts?: WithoutParser<string[]>): WithParser<string[]> => {
+    return isArray(isString, opts);
+}
+
+export const Env = {
+    number: isNumber,
+    boolean: isBoolean,
+    enum: isEnum,
+    json: isJson,
+    array: isArray,
+    stringArray: isStringArray
+};
---
+++ test/index.ts
@@ -0,0 +1,202 @@
+import { Env, parseDotEnv } from '../src';
+
+describe('dotenv:', () => {
+
+    beforeEach(() => {
+        for (const key of Object.keys(process.env)) {
+            delete process.env[key];
+        }
+    })
+
+    test('parses', () => {
+
+        process.env['A'] = 'a string';
+        process.env['B'] = 'a string';
+
+        const env = parseDotEnv({
+            A: {},
+            B: {}
+        });
+
+        expect(env.A).toBe('a string');
+        expect(env.B).toBe('a string');
+    });
+
+    test('catches required vars', () => {
+
+        process.env['A'] = 'a string';
+        process.env['B'] = 'a string';
+
+        const test = () => {
+            parseDotEnv({
+                A: {},
+                B: {},
+                C: {}
+            });
+        }
+
+        expect(test).toThrow();
+    });
+
+    test('parses numbers', () => {
+        process.env['A'] = '3.14159';
+        const env = parseDotEnv({
+            A: Env.number()
+        });
+
+        expect(env.A).toBe(3.14159);
+    });
+
+    test('throws on NaN', () => {
+        process.env['A'] = 'abcd';
+        const test = () => {
+            parseDotEnv({
+                A: Env.number()
+            })
+        };
+
+        expect(test).toThrow();
+    });
+
+    test('throws on number above and below min', () => {
+        process.env['A'] = '10';
+        const min = () => {
+            parseDotEnv({ A: Env.number({ min: 20 }) })
+        };
+        const max = () => {
+            parseDotEnv({ A: Env.number({ max: 0 }) })
+        };
+
+        expect(min).toThrow();
+        expect(max).toThrow();
+    });
+
+    test('numbers have default.', () => {
+        const env = parseDotEnv({ A: Env.number({ default: 42 }) })
+
+        expect(env.A).toBe(42);
+    });
+
+    test('parses booleans', () => {
+        process.env['A'] = 'true';
+        process.env['B'] = '1';
+        process.env['C'] = 'FALSE';
+        process.env['D'] = '0';
+        const env = parseDotEnv({
+            A: Env.boolean(),
+            B: Env.boolean(),
+            C: Env.boolean(),
+            D: Env.boolean(),
+            E: Env.boolean({ default: true })
+        });
+
+        expect(env.A).toBe(true);
+        expect(env.B).toBe(true);
+        expect(env.C).toBe(false);
+        expect(env.D).toBe(false);
+        expect(env.E).toBe(true);
+    });
+
+    test('throws on not a boolean', () => {
+        process.env['A'] = 'abcd';
+        const test = () => {
+            parseDotEnv({
+                A: Env.boolean()
+            })
+        };
+
+        expect(test).toThrow();
+    });
+
+    test('enumerations', () => {
+        process.env['A'] = 'DEBUG';
+        process.env['B'] = 'trace';
+
+        const env = parseDotEnv({
+            A: Env.enum(['DEBUG', 'TRACE', 'INFO', 'WARNING', 'ERROR', 'FATAL']),
+            B: Env.enum(['DEBUG', 'TRACE', 'INFO', 'WARNING', 'ERROR', 'FATAL'])
+
+        });
+
+        expect(env.A).toBe('DEBUG');
+        expect(env.B).toBe('TRACE');
+    });
+
+    test('enumerations throw', () => {
+        process.env['A'] = 'not debug';
+
+        const test = () => {
+            parseDotEnv({
+                A: Env.enum(['DEBUG', 'TRACE', 'INFO', 'WARNING', 'ERROR', 'FATAL']),
+            })
+        };
+
+        expect(test).toThrow();
+    });
+
+    test('enumerations default', () => {
+        const env =
+            parseDotEnv({
+                A: Env.enum(['DEBUG', 'TRACE', 'INFO', 'WARNING', 'ERROR', 'FATAL'], { default: 'INFO' }),
+            });
+
+        expect(env.A).toBe('INFO');
+    });
+
+    test('string array', () => {
+        process.env['A'] = '["foo", "bar", "baz"]';
+
+        const env = parseDotEnv({
+            A: Env.stringArray()
+        });
+
+        expect(env.A).toStrictEqual(['foo', 'bar', 'baz']);
+    });
+
+    test('fail string array, not all strings', () => {
+        process.env['A'] = '["foo", "bar", 42]';
+
+        const test = () => parseDotEnv({ A: Env.stringArray() });
+
+        expect(test).toThrow()
+    });
+
+    test('fail string array, not even array', () => {
+        process.env['A'] = '1066';
+
+        const test = () => parseDotEnv({ A: Env.stringArray() });
+
+        expect(test).toThrow()
+    });
+
+    test('json', () => {
+        process.env['A'] = '{ "foo": 42, "bar": "string", "baz": true }';
+        type MyObj = {
+            foo: number;
+            bar: string;
+            baz: boolean;
+        }
+        const isMyObj = (obj: any): obj is MyObj => {
+            if (
+                'foo' in obj && typeof obj.foo === 'number'
+                && 'bar' in obj && typeof obj.bar === 'string'
+                && 'baz' in obj && typeof obj.baz === 'boolean'
+            ) {
+                return true;
+            }
+            return false;
+        }
+
+        const env = parseDotEnv({ A: Env.json(isMyObj) });
+
+        expect(env.A.foo).toBe(42);
+        expect(env.A.bar).toBe('string');
+        expect(env.A.baz).toBe(true);
+
+
+        process.env['A'] = '{ "foo": 42, "bar": "string", "baz": "true" }';
+        const test = () => parseDotEnv({ A: Env.json(isMyObj) });
+        expect(test).toThrow();
+    });
+
+});
